938079683b8b5d91a931ae9c453a1b9ea1cc8008
trvinhky <vinhky26032002@gmail.com>
2022-08-02 08:52:57 +0700

    delete comment

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import { Provider } from 'react-redux'
 import store from './stores'
 import 'react-toastify/dist/ReactToastify.css'
 
-console.log(process.env.REACT_APP_BACKEND_URL)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
